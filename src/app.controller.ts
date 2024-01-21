e72463e23120cab8475eb44cfbba06a9857267ff
acristoni <adonai_cn@hotmail.com>
2024-01-21 07:43:27 -0300

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('/hello')
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('/hello')
+  postHello(@Body() { text }): string {
+    return this.appService.postHello(text);
+  }
+}
